bb07c483b651b55b68010900f49b7829c8fdd120
Valeriy Bobrov <bobrovv20@yandex.ru>
2023-05-17 14:12:24 +0300

    Maket 1.5.1

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
-new Swiper('.swiper', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: false
-    },
-    slidesPerView: 'auto',
-    spaceBetween: 16,
-});
+let init = false;
+
+function swiperCard() {
+    if (window.innerWidth <= 768) {
+        init = true;
+        swiper = new Swiper('.swiper', {
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: false
+            },
+            slidesPerView: 'auto',
+            spaceBetween: 16,
+        });
+    } else if (init) {
+        swiper.destroy();
+        init = false;
+    }
+}
+
+swiperCard();
+window.addEventListener('resize', swiperCard);
 
 let btnClose = document.querySelector('.buttonClose');
 
+
+
 let toggleList = function () {
-    let divList = document.querySelectorAll('.list-service__item--hidden');
+    let divList = document.querySelectorAll('.swiper-slide--hidden');
+
     for (let i = 0; i < divList.length; i++) {
         let element = divList[i];
         if (element.style.display === 'none') {
-            element.style.display = 'inherit';
-            btnClose.textContent = 'Скрыть'
+            element.style.display = 'block';
+            element.classList.add('swiper-slide');
+            btnClose.textContent = 'Скрыть';
+            btnClose.classList.add('buttonCloseRotate');
         } else {
             element.style.display = 'none';
             btnClose.textContent = 'Показать все';
+            btnClose.classList.remove('buttonCloseRotate');
         }
     }
 }
